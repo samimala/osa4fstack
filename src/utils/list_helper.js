5295a4cc453a2f74c43475cfaadb00977d706d2f
Sami Mäläskä <epsilon@welho.com>
2018-09-02 01:21:58 +0300

    Teht 4

--- src/utils/list_helper.js
+++ src/utils/list_helper.js
@@ -11,9 +11,31 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer,0)
 }
 
+const favoriteBlog = (blogs) => {
+  let maxLikes = blogs.reduce((accum,blog) => accum=Math.max(accum, blog.likes),0)
+  let maxIndex = blogs.findIndex((blog) => blog.likes === maxLikes)
+  return { title: blogs[maxIndex].title, author: blogs[maxIndex].author, likes: blogs[maxIndex].likes }
+}
+
+const mostBlogs = (blogs) => {
+  let authors = blogs.map(blog => blog.author) 
+  let blogCounts = []
+
+  authors.forEach(auth => {
+    if (!blogCounts.find(val => val.author===auth)) {
+      blogCounts.push({ author: auth, blogs: blogs.filter(blog => blog.author === auth).length })
+    }
+  })
+
+  let maxBlogs = blogCounts.reduce((accum,blogcnt) => accum=Math.max(accum, blogcnt.blogs),0)
+  let maxIndex = blogCounts.findIndex((blogcnt) => blogcnt.blogs === maxBlogs)
+  return blogCounts[maxIndex]
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
 
