e11cb56800a0470ba3bd5ec61e5f5659514ab5c8
Sami Mäläskä <epsilon@welho.com>
2018-09-11 02:44:01 +0300

    Teht 8

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.use(middleware.error)
 const server = http.createServer(app)
 
 server.listen(config.port, () => {
-  console.log('Server running on port ${config.port}')
+  console.log(`Server running on port ${config.port}`)
 })
 
 server.on('close', () => {
