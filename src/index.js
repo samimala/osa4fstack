ec381f52f11f7d90d615d13fe786abcced0397b4
Sami Mäläskä <epsilon@welho.com>
2018-09-29 23:59:19 +0300

    Teht 15

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
 
 
 
@@ -25,6 +26,7 @@ app.use(express.static('build'))
 app.use(middleware.logger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.error)
 
