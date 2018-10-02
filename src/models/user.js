0b1af4e6d53fe93beba421e57f608765c5177289
Sami Mäläskä <epsilon@welho.com>
2018-10-02 21:24:10 +0300

    Teht 17

--- src/models/user.js
+++ src/models/user.js
@@ -11,8 +11,20 @@ const userSchema = new mongoose.Schema({
   username: String,
   name: String,
   adult: Boolean,
-  passwordHash: String
+  passwordHash: String,
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Blog' }]
 })
+
+userSchema.statics.format = (user) => {
+  return {
+    id: user.id,
+    username: user.username,
+    name: user.name,
+    adult: user.adult,
+    blogs: user.blogs
+  }
+}
+
 
 const User = mongoose.model('User', userSchema)
 
