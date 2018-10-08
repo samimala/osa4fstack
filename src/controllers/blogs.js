a2e3d3536bc270329ffdebc4b36656c9731dd34e
Sami Mäläskä <epsilon@welho.com>
2018-10-09 00:33:01 +0300

    Teht 19

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,8 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -27,28 +34,41 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   if (body.title===undefined || body.url===undefined) {
-    response.status(400).end()
+    response.status(400).json({ error: 'missing essential content'})
     return
   }
 
-  //const user = await User.findById(body.userId)
-  const user = await User.findOne()
+  try {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error : 'Token missing or invalid' })
+    }
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    likes: (body.likes===undefined)?0:body.likes,
-    url: body.url,
-    user: user._id
-  })
+    const user = await User.findById(decodedToken.id)
 
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      likes: (body.likes===undefined)?0:body.likes,
+      url: body.url,
+      user: user._id
+    })
 
-  //console.log(request.body)
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-
-  response.status(201).json(savedBlog)
+    //console.log(request.body)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
+  }
+  catch (exception) {
+    if (exception.name === 'JsonWebTokenError' ) {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'something went wrong' })
+    }
+  }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
