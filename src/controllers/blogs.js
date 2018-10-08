6086711f6eebdab334bc5261173ef5501059bdd3
Sami Mäläskä <epsilon@welho.com>
2018-10-09 02:15:33 +0300

    Teht 20

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -3,13 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -39,9 +32,8 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   try {
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken || !decodedToken.id) {
       return response.status(401).json({ error : 'Token missing or invalid' })
     }
 
